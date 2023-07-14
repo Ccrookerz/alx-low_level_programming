26af598939d51797698bcc573208cd2e57f799b1
Nifemi <oluwaladenifemi4@gmail.com>
2023-07-14 14:07:34 -0700

    99

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,29 +14,33 @@ int main(void)
 	while (digit1 <= 9)
 	{
 		digit2 = 0;
-		while (digit3 <= 9)
+		while (digit2 <= 9)
 		{
-			if (digit1 != digit2 &&
-				digit1 < digit2 &&
-				digit2 != digit3 &&
-				digit2 < digit3)
+			digit3 = 0;
+			digit3 = 0;
+			while (digit 3 <= 9)
 			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
-				putchar(digit3 + 48);
-
-				if (digit1 + digit2 + digit3 != 24)
+				if (digit1 != digit2 &&
+					digit1 < digit2 &&
+					digit2 != digit3 &&
+					digit2 < digit3)
 				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			digit3++;
-		}
-		digit2++;
-	}
-	digit1++;
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
 
+					if (digit1 + digit2 + digit3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				digit3++;
+			}
+			digit2++;
+		}
+		digit1++;
+	}
 	putchar('\n');
 	return (0);
 }
