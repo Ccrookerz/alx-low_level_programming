3b332364f63d0deda52b0b9d8c261a3db604100a
Nifemi <oluwaladenifemi4@gmail.com>
2023-07-14 06:47:17 -0700

    new

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int n;
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
@@ -21,7 +22,7 @@ int main(void)
 	}
 	if (n == 0)
 	{
-		printf("%d is zero\n", n)
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
