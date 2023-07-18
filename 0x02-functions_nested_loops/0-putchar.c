91e1200db6ae91cdb09b43cbcb9c5b5a756fd973
Nifemi <oluwaladenifemi4@gmail.com>
2023-07-18 05:54:48 -0700

    #include main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-char str[] = "_putchar";
-int i = 0;
+	char str[] = "_putchar";
+	int i = 0;
 
-while (str[i] != '\0')
+	while (str[i] != '\0')
 	{
-	char c = str[i];
+		char c = str[i];
 
-	_putchar(c);
-	i++;
-	}
-return (0);
+		_putchar(c);
+		i++;
+	}	
+	
+	return (0);
 }
