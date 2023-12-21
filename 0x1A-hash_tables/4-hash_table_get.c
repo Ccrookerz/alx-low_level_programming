5b03a28c7905248ea7c0713181879f4a41b68d03
Nifemi <oluwaladen@gmail.com>
2023-12-21 04:12:12 -0800

    hash_tables tasks

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key
+ * Return: value associated with the element or NULL otherwise
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+hash_node_t *node;
+unsigned long int index;
+
+if (ht == NULL || key == NULL)
+return (NULL);
+
+index = key_index((const unsigned char *)key, ht->size);
+node = ht->array[index];
+while (node)
+{
+if (strcmp(node->key, key) == 0)
+return (node->value);
+node = node->next;
+}
+return (NULL);
+}
